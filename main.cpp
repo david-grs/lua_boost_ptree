0b3cc7826fb3192326be124787799a31552e4186
David G <david.a.grs@gmail.com>
2016-08-21 21:16:20 +0200

    calling fct from lua to c++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <lua5.2/lua.hpp>
 
 #include <iostream>
+#include <sstream>
 
 struct Script
 {
@@ -25,13 +26,17 @@ struct Script
         if (luaL_loadfile(mState, filename.c_str()) != LUA_OK)
             throw std::runtime_error("error while loading lua file: " + GetLastError());
 
+        lua_pushlightuserdata(mState, this);
+        lua_pushcclosure(mState, &Script::ReceiveHelper, 1);
+        lua_setglobal(mState, "send");
+
         if (lua_pcall(mState, 0, LUA_MULTRET, 0) != LUA_OK)
             throw std::runtime_error("error while running lua script: " + GetLastError());
     }
 
     void Call()
     {
-        lua_getglobal(mState, "foo");
+        lua_getglobal(mState, "recv");
         lua_pushnumber(mState, 1);
 
         if (lua_pcall(mState, 1, 1, 0 ) != 0)
@@ -39,6 +44,46 @@ struct Script
     }
 
 private:
+    int Receive(lua_State* lua_state)
+    {
+        std::stringstream str_buf;
+        while(lua_gettop(lua_state))
+        {
+            str_buf.str(std::string());
+            str_buf << " ";
+            switch(lua_type(lua_state, lua_gettop(lua_state)))
+            {
+            case LUA_TNUMBER:
+                str_buf << "script returned the number: "
+                    << lua_tonumber(lua_state, lua_gettop(lua_state));
+                break;
+            case LUA_TTABLE:
+                str_buf << "script returned a table";
+                break;
+            case LUA_TSTRING:
+                str_buf << "script returned the string: "
+                    << lua_tostring(lua_state, lua_gettop(lua_state));
+                break;
+            case LUA_TBOOLEAN:
+                str_buf << "script returned the boolean: "
+                    << lua_toboolean(lua_state, lua_gettop(lua_state));
+                break;
+            default:
+                str_buf << "script returned an unknown-type value";
+            }
+            lua_pop(lua_state, 1);
+            std::cout << str_buf.str() << std::endl;
+        }
+
+        return 0;
+    }
+
+    static int ReceiveHelper(lua_State* L)
+    {
+        Script* script = (Script*)lua_touserdata(L, lua_upvalueindex(1));
+        return script->Receive(L);
+    }
+
     std::string GetLastError()
     {
         std::string message = lua_tostring(mState, -1);
